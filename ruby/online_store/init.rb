94a6998cebeb139e68d2439bf7dfea61c0bf78fb
szrtlk <szeertlek@gmail.com>
2023-01-28 04:13:13 +0100

    Ruby and testing courses were updated, 2023-01-28

--- ruby/online_store/init.rb
+++ ruby/online_store/init.rb
@@ -6,9 +6,9 @@ Dir["*.rb"].each { |file| require_relative file if file != 'init.rb' }
 
 # создаём экземпляры класса Item и задаём для них аргументы (для первого экземпляра задан аргумент price, для второго - нет)
 
-item1 = VirtualItem.new({price: 25, name: 'Car'})
-item2 = RealItem.new({weight: 120, name: 'Car'})
-item3 = RealItem.new({price: 25, weight: 120, name: 'Car'})
+item1 = VirtualItem.new({price: 105, name: 'E-book'})
+item2 = RealItem.new({price: 90, weight: 120, name: 'Spoon'})
+item3 = RealItem.new({price: 120, weight: 120, name: 'Fork'})
 
 # создаём экземпляр класса Cart
 # используем для него методы класса Cart, при необходимости задаём аргументы
@@ -20,8 +20,8 @@ cart.add_item (item1)
 cart.add_item (item2)
 cart.add_item (item3)
 cart.delete_invalid_items
+p 'Items of cart'
 p cart
-p cart.items.size
 
 # создаём экземпляр класса Order
 # используем для него методы класса Order, при необходимости задаём аргументы
@@ -33,22 +33,38 @@ order.add_item (item1)
 order.add_item (item2)
 order.add_item (item3)
 order.delete_invalid_items
+puts
+p 'Items of order'
 p order
-p order.items.size
 
 # проверяем, отвечают ли данные объекты на аргумент weight
 
+puts
+p 'Weight presense of items'
 p item1.respond_to? :weight
 p item2.respond_to? :weight
 p item3.respond_to? :weight
 
 # используем метод класса
 
+puts
+p 'Discount'
 p Item.discount
 
 # вызовем метод final_price для объектов класса Item
 
+puts
+p 'Price of items'
 p item1.price
 p item3.price
+puts
+p 'Final price of items'
 p item1.final_price
 p item3.final_price
+
+# считаем количество заказов с непустым значением аргумента price
+
+puts
+p 'Number of valid items of cart and order'
+p cart.count_valid_items
+p order.count_valid_items
--- ruby/online_store/item_container.rb
+++ ruby/online_store/item_container.rb
@@ -1,27 +1,63 @@
 # создаём модуль ItemContainer
-# 1 метод класса добавляет новый элемент массива в конец
-# 2 метод класса удаляет последний элемент массива
-# 3 метод класса проверяет, есть ли элемента массива со значением price == 0
-# 4 массив удаляет такие элементы массива
 
 module ItemContainer
 
-  def add_item(item)
-    @items.push item
-  end
+  # создаём вложенный модуль
+  # объявляем переменную MinPrice
+  # метод класса задаёт значение условия, которое задаёт переменная MinPrice
+  module ClassMethods
 
-  def remove_item
-    @items.pop
-  end
+    MinPrice = 100
 
-  def validate
-    @items.each do |i|
-      puts 'Item has no price' if i.price.nil?
+    def min_price
+      MinPrice
     end
+
   end
 
-  def delete_invalid_items
-    @items.delete_if {|i| i.price.nil?}
+  # создаём вложенный модуль
+  # 1 метод класса добавляет новый элемент массива в конец при соблюдении условия
+  # это условие задано с помощью метода min_price и действует для классов, использующих модуль
+  # 2 метод класса удаляет последний элемент массива
+  # 3 метод класса проверяет, есть ли элемента массива со значением price == 0
+  # 4 массив удаляет такие элементы массива
+  # 5 массив считает количество элементов с непустым значением аргумента price
+
+  module InstanceMethods
+
+    def add_item(item)
+      @items.push item unless item.price < self.class.min_price
+    end
+
+    def remove_item
+      @items.pop
+    end
+
+    def validate
+      @items.each do |i|
+        puts 'Item has no price' if i.price.nil?
+      end
+    end
+
+    def delete_invalid_items
+      @items.delete_if {|i| i.price.nil?}
+    end
+
+    def count_valid_items
+      @items.count { |i| i.price }
+    end
+
+  end
+
+  # создаём callback на include модулей
+  # он позволяет добавлять ClassMethods, данные методы будут доступны непосредственно через класс
+  # он позволяет добавлять InstanceNethods, данные методы будут добавляться как экземпляр класса
+  # для InstanceMethods можно также использовать команду classes.send :include, InstanceMethods
+  # таким образом, при вызове модуля ItemContainer автоматически будут вызваны вложенные модули
+
+  def self.included (classes)
+    classes.extend ClassMethods
+    classes.class_eval { include InstanceMethods }
   end
 
 end
